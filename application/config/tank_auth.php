2a5e20d5d49e9a4d8b62d5544601a0801978b675
Shawn Plummer <smplummer@gmail.com>
2012-01-22 17:38:55 -0500

    turn off email auth cause it doesn't seem to work locally

--- application/config/tank_auth.php
+++ application/config/tank_auth.php
@@ -41,7 +41,7 @@ $config['phpass_hash_strength'] = 8;
 */
 $config['allow_registration'] = TRUE;
 $config['captcha_registration'] = TRUE;
-$config['email_activation'] = TRUE;
+$config['email_activation'] = FALSE;
 $config['email_activation_expire'] = 60*60*24*2;
 $config['email_account_details'] = TRUE;
 $config['use_username'] = TRUE;
