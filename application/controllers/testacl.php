87db83589d62f8f1979604964c22f7cd74704969
Shawn Plummer <smplummer@gmail.com>
2012-01-29 20:29:05 -0500

    created the cal model and am testing it now in testacl

--- application/controllers/testacl.php
+++ application/controllers/testacl.php
@@ -5,42 +5,43 @@ class Testacl extends CI_Controller
 	function  __construct()
 	{
 		parent::__construct();
-		$this->load->library('acl');
+		$this->load->library('zacl');
 		define('ROLE', '7');
 		define('RESOURCE', '3');
  	}
 
- function index() {
- echo '<pre>' ;
- print_r($this->acl);
- echo '</pre>';
-  if (!$this->acl->can_read(ROLE, RESOURCE)) {
-   die("You do not have permissions to read this resource");
-  }
-  echo "You have permission to read this resource!";
- }
- function write() {
-  if (!$this->acl->can_write(ROLE, RESOURCE)) {
-   die("You do not have permissions to write to this resource");
-  }
-  echo "You have permission to write to this resource!";
- }
- function modify() {
-  if (!$this->acl->can_modify(ROLE, RESOURCE)) {
-   die("You do not have permissions to modify this resource");
-  }
-  echo "You have permission to modify this resource!";
- }
- function delete() {
-  if (!$this->acl->can_delete(ROLE, RESOURCE)) {
-   die("You do not have permissions to delete this resource");
-  }
-  echo "You have permission to delete this resource!";
- }
- function publish() {
-  if (!$this->acl->can_publish(ROLE, RESOURCE)) {
-   die("You do not have permissions to publish this resource");
-  }
-  echo "You have permission to publish this resource!";
- }
+
+	function index() {
+		if (!$this->zacl->can_read(ROLE, RESOURCE)) {
+			die("You do not have permissions to read this resource");
+		}
+			echo "You have permission to read this resource!";
+			echo '<pre>';
+			print_r($this->acl_model->get_user_roles('3'));
+			echo '</pre>';
+	}
+	function write() {
+		if (!$this->zacl->can_write(ROLE, RESOURCE)) {
+			die("You do not have permissions to write to this resource");
+		}
+			echo "You have permission to write to this resource!";
+	}
+	function modify() {
+		if (!$this->zacl->can_modify(ROLE, RESOURCE)) {
+			die("You do not have permissions to modify this resource");
+		}
+			echo "You have permission to modify this resource!";
+	}
+	function delete() {
+		if (!$this->zacl->can_delete(ROLE, RESOURCE)) {
+			die("You do not have permissions to delete this resource");
+		}
+			echo "You have permission to delete this resource!";
+	}
+	function publish() {
+		if (!$this->zacl->can_publish(ROLE, RESOURCE)) {
+			die("You do not have permissions to publish this resource");
+		}
+			echo "You have permission to publish this resource!";
+	}
 }
---
+++ application/models/acl_model.php
@@ -0,0 +1,103 @@
+<?php if( ! defined('BASEPATH') ) exit('No direct script access allowed');
+
+/**
+ * SRA - acl_model Model Class
+ *
+ * Functions for getting and setting acls
+ *
+ * @author	Shawn Plummer
+ */
+
+class Acl_model extends CI_Model 
+{
+	private $acl_permissions_table	= 'acl_permissions';	// Permissions
+	private $acl_resources_table	= 'acl_resources';	// Resources
+	private $acl_roles_table		= 'acl_roles';	// Roles
+	private $user_role_lookup_table	= 'user_role_lookup';	// Ties user to role
+	
+
+	/**
+	 * get_user_roles- get all of a given users roles, takes user_id and returns
+	 * object of all the users roles
+	 * 
+	 * @param	- int
+	 * @return	- object
+	 * 
+	 */
+	function get_user_roles($user_id)
+	{
+		$this->db->where('fk_user_id', $user_id);
+		$this->db->order_by('fk_acl_roles_id');
+		$query = $this->db->get($this->user_role_lookup_table);
+		if ($query->num_rows() >0) 
+		{ 
+			return $query->result();
+		} 
+		return NULL;
+	}
+
+	/**
+	 * add_user_role - Insert the user_id and role_id to the user_role_lookup table
+	 * 
+	 * @param	 - int
+	 * @param	 - int
+	 * @return	 - object
+	 * 
+	 */
+	function add_user_role($user_id, $role_id)
+	{
+		$this->db->set('fk_user_id', $user_id);
+		$this->db->set('fk_acl_roles_id', $role_id);
+		return $this->db->insert($this->user_role_lookup_table);
+	}
+
+	/**
+	 * add_resource - Add a record to the resource table and return the new acl_resource_id
+	 * 
+	 * @param	 - array
+	 * @return	 - array
+	 * 
+	 */
+	function add_resource($data)
+	{
+		$this->db->insert($this->acl_resources_table, $data);
+		$acl_resource_id = $this->db->insert_id();
+		return array('acl_resource_id' => $acl_resource_id);
+	}
+
+
+	/**
+	 * delete_user_role - Delete the user_id and role_id to the user_role_lookup table
+	 * 
+	 * @param	 - int
+	 * @param	 - int
+	 * @return	 - object
+	 * 
+	 */
+	function delete_user_role($user_id, $role_id)
+	{
+		$this->db->where('fk_user_id', $user_id);
+		$this->db->where('fk_acl_roles_id', $role_id);
+		return $this->db->delete($this->user_role_lookup_table);
+	}
+
+	/**
+	 * delete_all_user_roles Name - Delete all roles for given user_id
+	 * 
+	 * @param	 - int
+	 * @param	 - int
+	 * @return	 - object
+	 * 
+	 */
+	function delete_all_user_roles($user_id, $role_id)
+	{
+		$this->db->where('fk_user_id', $user_id);
+		return $this->db->delete($this->user_role_lookup_table);
+	}
+
+
+
+}
+
+/* End of file acl_model.php */
+/* Location: ./application/models/acl_model.php */
