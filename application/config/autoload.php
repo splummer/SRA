18db95a890204fd7ab4f8e6801e5ba9d70bf9e88
Shawn Plummer <smplummer@gmail.com>
2012-01-29 22:00:11 -0500

    included the zacl library

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'tank_auth', 'session');
+$autoload['libraries'] = array('database', 'tank_auth', 'session', 'zacl');
 
 
 /*
