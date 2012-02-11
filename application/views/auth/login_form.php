c92a270df4bc0e6f69e8e8056e89ad6da57e57d8
Shawn Plummer <smplummer@gmail.com>
2012-02-11 17:56:21 -0500

    Fixed login form for bootstrap 2.0

--- application/views/auth/login_form.php
+++ application/views/auth/login_form.php
@@ -5,7 +5,7 @@ $login = array(
 	'value' => set_value('login'),
 	'maxlength'	=> 80,
 	'size'	=> 30,
-	'class' => 'span4',
+	'class' => 'focused',
 );
 if ($login_by_username AND $login_by_email) {
 	$login_label = 'Email or Username';
@@ -30,31 +30,32 @@ $captcha = array(
 	'id'	=> 'captcha',
 	'maxlength'	=> 8,
 );
+$form_label_param = array(
+	'class'	=>	'control-label'
+);
 ?>
-<div class="row">
-	<div class="span4">
+<div class="row-fluid">
+	<div class="span2">
 		&nbsp;
 	</div>
-	<div class="span12">
-		<?php echo form_open($this->uri->uri_string()); ?>
-				<?php echo isset($errors[$login['name']]) || form_error($login['name']) ? '<div class="clearfix error">' : '<div class="clearfix">' ; ?>
-					<?php echo form_label($login_label, $login['id']); ?>
-					<div class="input">
-						<div class=input-append>
-						<?php echo form_input($login); ?><label class="add-on active"><?php echo form_checkbox($remember); ?>&nbsp;Remember Me</label>
+	<div class="span10">
+		<?php echo form_open($this->uri->uri_string() ); ?>
+				<?php echo isset($errors[$login['name']]) || form_error($login['name']) ? '<div class="control-group error">' : '<div class="control-group">' ; ?>
+					<?php echo form_label($login_label, $login['id'], $form_label_param); ?>
+					<div class="controls">
+						<?php echo form_input($login); ?>
 						<?php echo form_error($login['name'], '<span class="help-inline">', '</span>') ; ?>
 						<?php echo isset($errors[$login['name']]) ? '<span class="help-inline">' . $errors[$login['name']] . '</span>' : '' ; ?>
-						</div>
 					</div>			
-				</div><!-- /clearfix-->
-				<?php echo isset($errors[$password['name']]) || form_error($password['name']) ? '<div class="clearfix error">' : '<div class="clearfix">' ; ?>
-					<?php echo form_label('Password', $password['id']); ?>
-					<div class="input">
+				</div><!-- /control-group-->
+				<?php echo isset($errors[$password['name']]) || form_error($password['name']) ? '<div class="control-group error">' : '<div class="control-group">' ; ?>
+					<?php echo form_label('Password', $password['id'], $form_label_param); ?>
+					<div class="controls">
 						<?php echo form_password($password); ?>
 						<?php echo form_error($password['name'], '<span class="help-inline">', '</span>'); ?>
 						<?php echo isset($errors[$password['name']])? '<span class="help-inline">' .  $errors[$password['name']] . '</span>' : '' ; ?>
 					</div>
-				</div><!-- /clearfix-->
+				</div><!-- /control-group-->
 				<?php if ($show_captcha) { 
 					if ($use_recaptcha) { ?>
 						<div id="recaptcha_image"></div>
@@ -63,48 +64,43 @@ $captcha = array(
 						<div class="recaptcha_only_if_audio"><a href="javascript:Recaptcha.switch_type('image')">Get an image CAPTCHA</a></div>
 						<div class="recaptcha_only_if_image">Enter the words above</div>
 						<div class="recaptcha_only_if_audio">Enter the numbers you hear</div>
-						<input type="text" id="recaptcha_response_field" name="recaptcha_response_field" />
+						<controls type="text" id="recaptcha_response_field" name="recaptcha_response_field" />
 						<?php echo form_error('recaptcha_response_field'); ?>
 						<?php echo $recaptcha_html; ?>
 					<?php } else { ?>
-						<div class="clearfix">
-							<div class="input">
+						<div class="control-group">
+							<div class="controls">
 								<p>Enter the code exactly as it appears:</p>
 								<?php echo $captcha_html; ?>
 							</div>
-						</div><!-- /clearfix-->
+						</div><!-- /control-group-->
 						<?php $captch_error=form_error($captcha['name'], '<span class="help-inline">', '</span>') ; ?>
-						<?php echo isset($captch_error) ? '<div class="clearfix error">' : '<div class="clearfix">' ; ?>
-							<?php echo form_label('Confirmation Code', $captcha['id']); ?>
-								<div class="input">
+						<?php echo isset($captch_error) ? '<div class="control-group error">' : '<div class="control-group">' ; ?>
+							<?php echo form_label('Confirmation Code', $captcha['id'], $form_label_param); ?>
+								<div class="controls">
 								<?php echo form_input($captcha); ?>
 								<?php echo form_error($captcha['name'], '<span class="help-inline">', '</span>'); ?>
 							</div>
-						</div><!-- /clearfix-->
+						</div><!-- /control-group-->
 						<?php }
 					} ?>
-				<div class=clearfix>
-					<?php echo form_label('Remember Me', $remember['id']); ?>
-					<div class="input">
-						<ul class="inputs-list">
-							<li>
-								<label>
-									<?php echo form_checkbox($remember); ?>
-									<span></span>
-								</label>
-							</li>
-						</ul>
+				<div class=control-group>
+					<?php echo form_label('Remember Me', $remember['id'], $form_label_param); ?>
+					<div class="controls">
+						<label class="checkbox inline">
+							<?php echo form_checkbox($remember); ?>
+						</label>
 					</div>
-				</div><!-- /clearfix-->
-				<div class="input">
+				</div><!-- /control-group-->
+				<div class="controls">
 					<?php echo anchor('/auth/forgot_password/', 'Forgot password'); ?>
 					<?php if ($this->config->item('allow_registration', 'tank_auth')) echo anchor('/auth/register/', 'Register'); ?>
 				</div>
-				<div class="clearfix">
+				<div class="control-group">
 					<div class="actions">
 					<?php echo form_submit('submit', 'Login', 'class="btn primary"'); ?>
 					</div>
-				</div><!-- /clearfix -->
+				</div><!-- /control-group -->
 				<?php echo form_close(); ?>
-		</div><!-- /span12 -->
-</div><!-- /row -->
+		</div><!-- /span10 -->
+</div><!-- /row-fluid -->
